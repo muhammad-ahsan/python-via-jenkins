332392a29c4498c9b783bd914351a3c8a151b8ef
Ahsan <muhammad.ahsan@gmail.com>
2023-03-04 15:45:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-
 import socket
 
 
 def main() -> None:
-    """Python function to display the IP of the client where code is executed"""
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
     print(f"Hostname: {hostname} with IP Address: {ip_address}")
